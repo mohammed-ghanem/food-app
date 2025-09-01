d1e47271b8daaa4ab3066c3a631a92f641b364cd
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2025-09-01 23:40:29 +0300

    first commit

---
+++ components/categories/Categories.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { ActCategories } from "@/store/categories/categoriesSlice";
+import { useEffect } from "react";
+import Link from "next/link";
+// translate
+import LangUseParams from "@/translate/LangUseParams";
+import TranslateHook from "@/translate/TranslateHook";
+
+
+
+const Categories = () => {
+   const lang = LangUseParams() // Access dynamic [lang] parameter
+  const translate = TranslateHook(); // Access dynamic [lang] parameter
+
+  const dispatch = useAppDispatch();
+  const { loading, record, error } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(ActCategories());
+  }, [dispatch]);
+
+  const categoriesList =
+    loading === "pending" || loading === "idle" ? (
+      <div> {translate ? translate.loading.title : ""}</div>
+    ) : loading === "failed" ? (
+      <div>{error || "Error fetching categories"}</div>
+    ) : record.length > 0 ? (
+      record.map((category) => (
+        <div key={category.id}>
+          <Link href={`/${lang}/products/${category.id}`}>
+            {category.name}
+          </Link>
+
+        </div>
+      ))
+    ) : (
+      <div>No categories</div>
+    );
+
+  return <div>{categoriesList}</div>;
+};
+
+export default Categories;
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [ "https://placehold.co", "i.imgur.com" , "placeimg.com" , "placehold.co" , "julio.com" , "http2.mlstatic.com"], // add imgur domain here
+  },
+};
+
+export default nextConfig;
